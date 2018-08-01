6601fb54fbdf7bbe75bc1673382f1ac6a2a36476
Nicole Pretorius <nicole.pretoriusuk+learn@gmail.com>
2018-08-01 15:01:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,7 @@
 def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+    return "FizzBuzz"
+  end
 
   if int % 5 == 0
    return "Buzz"
@@ -6,10 +9,6 @@ def fizzbuzz(int)
 
   if int % 3 == 0
    return "Fizz"
-  end
-
-  if int % 15 == 0
-    return "FizzBuzz"
   end
 
 end
